731bd4a89c98009550c130591d0f95d5223c49a5
Yashodhan Devdhar <yashodhandevdhar@gmail.com>
2025-02-25 07:33:57 +0530

    wrapped with queryClient

--- 22-02-2025/ecommerce-app/src/main.tsx
+++ 22-02-2025/ecommerce-app/src/main.tsx
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 )
